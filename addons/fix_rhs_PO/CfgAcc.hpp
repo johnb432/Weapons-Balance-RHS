7003c7676a9f4ddca7f61fbe40bba6c9ebb857ee
johnb432 <58661205+johnb432@users.noreply.github.com>
2020-09-13 18:45:13 +0200

    Initial commit

---
+++ addons/fix_rhs_PO/CfgAcc.hpp
@@ -0,0 +1,255 @@
+class asdg_SlotInfo;
+
+class UnderBarrelSlot;
+class UnderBarrelSlot_rail: UnderBarrelSlot {
+    class compatibleItems {
+        rhs_acc_harris_swivel = 1;
+        rhsusf_acc_harris_bipod = 1;
+    };
+};
+class nia_rifle_grips_slot: UnderBarrelSlot {
+    displayName = "Gripod slot";
+    class compatibleItems {
+        rhsusf_acc_grip1 = 1;
+        rhsusf_acc_grip2 = 1;
+        rhsusf_acc_grip2_tan = 1;
+        rhsusf_acc_grip2_wd = 1;
+        rhsusf_acc_grip3 = 1;
+        rhsusf_acc_grip3_tan = 1;
+        rhs_acc_grip_rk2 = 1;
+        rhs_acc_grip_rk6 = 1;
+        rhs_acc_grip_ffg2 = 1;
+        rhsusf_acc_rvg_blk = 1;
+        rhsusf_acc_rvg_de = 1;
+        rhsusf_acc_tacsac_blk = 1;
+        rhsusf_acc_tacsac_tan = 1;
+        rhsusf_acc_tacsac_blue = 1;
+        rhsusf_acc_tdstubby_blk = 1;
+        rhsusf_acc_tdstubby_tan = 1;
+        rhsusf_acc_kac_grip = 1;
+        rhsusf_acc_harris_bipod = 1;
+        rhs_acc_harris_swivel = 1;
+    };
+};
+
+class asdg_UnderSlot: asdg_SlotInfo {
+    class compatibleItems {};
+};
+class nia_rifle_bipodsgrips_slot: asdg_UnderSlot {
+    class compatibleItems: compatibleItems {
+        rhsusf_acc_grip1 = 1;
+        rhsusf_acc_grip2 = 1;
+        rhsusf_acc_grip2_tan = 1;
+        rhsusf_acc_grip2_wd = 1;
+        rhsusf_acc_grip3 = 1;
+        rhsusf_acc_grip3_tan = 1;
+        rhs_acc_grip_rk2 = 1;
+        rhs_acc_grip_rk6 = 1;
+        rhs_acc_grip_ffg2 = 1;
+        rhsusf_acc_rvg_blk = 1;
+        rhsusf_acc_rvg_de = 1;
+        rhsusf_acc_tacsac_blk = 1;
+        rhsusf_acc_tacsac_tan = 1;
+        rhsusf_acc_tacsac_blue = 1;
+        rhsusf_acc_tdstubby_blk = 1;
+        rhsusf_acc_tdstubby_tan = 1;
+        rhsusf_acc_kac_grip = 1;
+    };
+};
+class fk_rifle_grips_slot: asdg_SlotInfo {
+    iconPicture = "\a3\weapons_f_mark\Data\UI\attachment_under";
+    iconPinpoint = "Bottom";
+    linkProxy = "\a3\data_f_mark\Proxies\Weapon_Slots\UNDERBARREL";
+    class compatibleItems {
+        rhs_acc_grip_ffg2 = 1;
+        rhs_acc_grip_rk2 = 1;
+        rhs_acc_grip_rk6 = 1;
+        rhsusf_acc_grip1 = 1;
+        rhsusf_acc_grip2 = 1;
+        rhsusf_acc_grip2_tan = 1;
+        rhsusf_acc_grip2_wd = 1;
+        rhsusf_acc_grip3 = 1;
+        rhsusf_acc_grip3_tan = 1;
+        rhsusf_acc_kac_grip = 1;
+        rhsusf_acc_rvg_blk = 1;
+        rhsusf_acc_rvg_de = 1;
+        rhsusf_acc_tacsac_blk = 1;
+        rhsusf_acc_tacsac_blue = 1;
+        rhsusf_acc_tacsac_tan = 1;
+        rhsusf_acc_tdstubby_blk = 1;
+        rhsusf_acc_tdstubby_tan = 1;
+    };
+};
+
+class rhs_western_rifle_gripod_slot : rhs_rifle_gripod_slot {
+    class compatibleItems: compatibleItems {
+        rhsusf_acc_harris_bipod = 1;
+        rhs_acc_harris_swivel = 1;
+    };
+};
+
+class rhs_western_mg_gripod_slot: rhs_western_rifle_gripod_slot {
+    class compatibleItems: compatibleItems {
+        rhsusf_acc_grip2 = 1;
+        rhsusf_acc_grip2_tan = 1;
+        rhsusf_acc_grip2_wd = 1;
+    };
+};
+
+class rhs_western_m14_underbarrel_slot: UnderBarrelSlot {
+    class compatibleItems {
+        rhs_acc_harris_swivel = 1;
+    };
+};
+
+
+class PointerSlot;
+class PointerSlot_rail: PointerSlot {
+    class compatibleItems {
+        rhs_acc_2dpzenit_ris = 1;
+        rhs_acc_perst1ik_ris = 1;
+        rhs_acc_perst3 = 1;
+        rhsusf_acc_anpeq15side = 1;
+        rhsusf_acc_anpeq15_top = 1;
+        rhsusf_acc_anpeq15_wmx = 1;
+        rhsusf_acc_anpeq15_wmx_light = 1;
+        rhsusf_acc_anpeq15side_bk = 1;
+        rhsusf_acc_anpeq15_bk_top = 1;
+        rhsusf_acc_anpeq15 = 1;
+        rhsusf_acc_anpeq15_light = 1;
+        rhsusf_acc_anpeq15_bk = 1;
+        rhsusf_acc_anpeq15_bk_light = 1;
+        rhsusf_acc_anpeq15a = 1;
+        rhsusf_acc_anpeq16a = 1;
+        rhsusf_acc_anpeq16a_light = 1;
+        rhsusf_acc_anpeq16a_top = 1;
+        rhsusf_acc_anpeq16a_light_top = 1;
+        rhsusf_acc_m952v = 1;
+        rhsusf_acc_wmx = 1;
+        rhsusf_acc_wmx_bk = 1;
+    };
+};
+
+class CowsSlot;
+class CowsSlot_Rail: CowsSlot {
+    class compatibleItems {
+        rhs_acc_rakurspm = 1;
+        rhs_acc_1p87 = 1;
+        rhs_acc_dh520x56 = 1;
+        rhs_acc_ekp8_18 = 1;
+        rhs_acc_okp7_picatinny = 1;
+        rhsusf_acc_anpas13gv1 = 1;
+        rhsusf_acc_acog2_usmc = 1;
+        rhsusf_acc_acog3_usmc = 1;
+        rhsusf_acc_acog_usmc = 1;
+        rhsusf_acc_anpvs27 = 1;
+        rhsusf_acc_eotech = 1;
+        rhsusf_acc_g33_t1 = 1;
+        rhsusf_acc_g33_xps3 = 1;
+        rhsusf_acc_g33_xps3_tan = 1;
+        rhsusf_acc_elcan = 1;
+        rhsusf_acc_elcan_ard = 1;
+        rhsusf_acc_acog = 1;
+        rhsusf_acc_acog2 = 1;
+        rhsusf_acc_acog3 = 1;
+        rhsusf_acc_m2a1 = 1;
+        rhsusf_acc_eotech_552 = 1;
+        rhsusf_acc_eotech_552_d = 1;
+        rhsusf_acc_eotech_552_wd = 1;
+        rhsusf_acc_compm4 = 1;
+        rhsusf_acc_m8541 = 1;
+        rhsusf_acc_m8541_low = 1;
+        rhsusf_acc_m8541_low_d = 1;
+        rhsusf_acc_m8541_low_wd = 1;
+        rhsusf_acc_m8541_mrds = 1;
+        rhsusf_acc_premier_low = 1;
+        rhsusf_acc_premier = 1;
+        rhsusf_acc_premier_mrds = 1;
+        rhsusf_acc_leupoldmk4 = 1;
+        rhsusf_acc_leupoldmk4_2 = 1;
+        rhsusf_acc_leupoldmk4_d = 1;
+        rhsusf_acc_leupoldmk4_wd = 1;
+        rhsusf_acc_leupoldmk4_2_d = 1;
+        rhsusf_acc_leupoldmk4_2_mrds = 1;
+        rhsusf_acc_mrds = 1;
+        rhsusf_acc_mrds_c = 1;
+        rhsusf_acc_rm05 = 1;
+        rhsusf_acc_rx01_nofilter = 1;
+        rhsusf_acc_rx01 = 1;
+        rhsusf_acc_rx01_nofilter_tan = 1;
+        rhsusf_acc_rx01_tan = 1;
+        rhsusf_acc_su230 = 1;
+        rhsusf_acc_su230_c = 1;
+        rhsusf_acc_su230_mrds = 1;
+        rhsusf_acc_su230_mrds_c = 1;
+        rhsusf_acc_su230a = 1;
+        rhsusf_acc_su230a_c = 1;
+        rhsusf_acc_su230a_mrds = 1;
+        rhsusf_acc_su230a_mrds_c = 1;
+        rhsusf_acc_acog_mdo = 1;
+        rhsusf_acc_t1_high = 1;
+        rhsusf_acc_t1_low = 1;
+        rhsusf_acc_acog_rmr = 1;
+        rhsusf_acc_acog_d = 1;
+        rhsusf_acc_acog_wd = 1;
+        rhsusf_acc_eotech_xps3 = 1;
+        rhsgref_acc_rx01_nofilter_camo = 1;
+        rhsgref_acc_rx01_camo = 1;
+    };
+};
+
+class asdg_MuzzleSlot: asdg_SlotInfo {
+    class compatibleItems {};
+};
+
+class asdg_MuzzleSlot_762: asdg_MuzzleSlot {
+    class compatibleItems {
+        rhsusf_acc_aac_m14dcqd_silencer = 1;
+        rhsusf_acc_aac_m14dcqd_silencer_d = 1;
+        rhsusf_acc_aac_m14dcqd_silencer_wd = 1;
+        rhsusf_acc_m14_flashsuppresor = 1;
+    };
+};
+
+class asdg_MuzzleSlot_762R: asdg_MuzzleSlot {
+    class compatibleItems {
+        rhs_acc_dtkakm = 1;
+        rhs_acc_dtk1l = 1;
+    };
+};
+
+class asdg_MuzzleSlot_556_3CB_HK33: asdg_MuzzleSlot {
+    class compatibleItems {
+        rhsusf_acc_nt4_black = 1;
+        rhsusf_acc_nt4_tan = 1;
+        rhsusf_acc_rotex5_grey = 1;
+        rhsusf_acc_rotex5_tan = 1;
+        rhsusf_acc_sf3p556 = 1;
+        rhsusf_acc_sfmb556 = 1;
+    };
+};
+
+class asdg_MuzzleSlot_762_3CB_G3: asdg_MuzzleSlot {
+    class compatibleItems {
+        rhsusf_acc_aac_762sd_silencer = 1;
+        rhsusf_acc_aac_762sdn6_silencer = 1;
+        rhsusf_acc_aac_scarh_silencer = 1;
+        rhsusf_acc_aac_m14dcqd_silencer = 1;
+        rhsusf_acc_aac_m14dcqd_silencer_d = 1;
+        rhsusf_acc_aac_m14dcqd_silencer_wd = 1;
+        rhsusf_acc_m14_flashsuppresor = 1;
+        rhsgref_sdn6_suppressor = 1;
+    };
+};
+class asdg_MuzzleSlot_762_3CB_M14: asdg_MuzzleSlot {
+    class compatibleItems {
+        rhsusf_acc_aac_762sd_silencer = 1;
+        rhsusf_acc_aac_762sdn6_silencer = 1;
+        rhsusf_acc_aac_scarh_silencer = 1;
+        rhsusf_acc_aac_m14dcqd_silencer = 1;
+        rhsusf_acc_aac_m14dcqd_silencer_d = 1;
+        rhsusf_acc_aac_m14dcqd_silencer_wd = 1;
+        rhsusf_acc_m14_flashsuppresor = 1;
+        rhsgref_sdn6_suppressor = 1;
+    };
+};
---
+++ addons/fix_rhs_PO/CfgMagazineWells.hpp
@@ -0,0 +1,152 @@
+class CfgMagazineWells {
+    class CBA_12g_8rnds {
+        WB_Magazines[] = {
+            "rhsusf_8Rnd_00Buck",
+            "rhsusf_8Rnd_Slug",
+            "rhsusf_8Rnd_FRAG",
+            "rhsusf_8Rnd_HE",
+            "rhsusf_8rnd_doomsday_buck"
+        };
+    };
+
+    class CBA_300WM_AI {
+        WB_Magazines[] = {
+            "rhsusf_5Rnd_300winmag_xm2010"
+        };
+    };
+
+    class CBA_40mm_GP {
+        WB_Magazines[] = {
+            "rhs_VOG25",
+            "rhs_VOG25p",
+            "rhs_vg40tb",
+            "rhs_vg40sz",
+            "rhs_vg40op_white",
+            "rhs_vg40op_green",
+            "rhs_vg40op_red",
+            "rhs_GRD40_white",
+            "rhs_GRD40_green",
+            "rhs_GRD40_red",
+            "rhs_VG40MD_White",
+            "rhs_VG40MD_Green",
+            "rhs_VG40MD_Red",
+            "rhs_GDM40"
+        };
+    };
+
+    class CBA_40mm_M203 {
+        WB_Magazines[] = {
+            "rhs_mag_M441_HE",
+            "rhs_mag_M433_HEDP",
+            "rhs_mag_M781_Practice",
+            "rhs_mag_M397_HET",
+            "rhs_mag_M4009",
+            "rhs_mag_m576",
+            "rhs_mag_M585_white",
+            "rhs_mag_M661_green",
+            "rhs_mag_M662_red",
+            "rhs_mag_M713_red",
+            "rhs_mag_M714_white",
+            "rhs_mag_M715_green",
+            "rhs_mag_M716_yellow"
+        };
+    };
+
+    class CBA_556x45_MINIMI {
+        WB_Magazines[] = {
+            "rhs_200rnd_556x45_T_SAW"
+        };
+    };
+
+    class CBA_762x51_AICS {
+        WB_Magazines[] = {
+            "rhsusf_10Rnd_762x51_m118_special_Mag",
+            "rhsusf_10Rnd_762x51_m62_Mag",
+            "rhsusf_10Rnd_762x51_m993_Mag",
+            "rhsusf_5Rnd_762x51_AICS_m118_special_Mag",
+            "rhsusf_5Rnd_762x51_AICS_m62_Mag",
+            "rhsusf_5Rnd_762x51_AICS_m993_Mag"
+        };
+    };
+
+    class CBA_762x51_LINKS {
+        WB_Magazines[] = {
+            "rhsusf_50Rnd_762x51",
+            "rhsusf_50Rnd_762x51_m61_ap",
+            "rhsusf_50Rnd_762x51_m62_tracer",
+            "rhsusf_50Rnd_762x51_m80a1epr",
+            "rhsusf_50Rnd_762x51_m82_blank",
+            "rhsusf_50Rnd_762x51_m993",
+            "rhsusf_100Rnd_762x51",
+            "rhsusf_100Rnd_762x51_m61_ap",
+            "rhsusf_100Rnd_762x51_m62_tracer",
+            "rhsusf_100Rnd_762x51_m80a1epr",
+            "rhsusf_100Rnd_762x51_m82_blank",
+            "rhsusf_100Rnd_762x51_m993"
+        };
+    };
+
+    class CBA_762x51_SR25 {
+        WB_Magazines[] = {
+            "rhsusf_20Rnd_762x51_SR25_m118_special_Mag",
+            "rhsusf_20Rnd_762x51_SR25_mk316_special_Mag",
+            "rhsusf_20Rnd_762x51_SR25_m993_Mag",
+            "rhsusf_20Rnd_762x51_SR25_m62_Mag"
+        };
+    };
+
+    class CBA_762x54R_LINKS {
+        WB_Magazines[] = {
+            "rhs_100Rnd_762x54mmR",
+            "rhs_100Rnd_762x54mmR_green",
+            "rhs_100Rnd_762x54mmR_7N13",
+            "rhs_100Rnd_762x54mmR_7N26",
+            "rhs_100Rnd_762x54mmR_7BZ3",
+            "rhssaf_250Rnd_762x54R"
+        };
+    };
+
+    class CBA_792x57_LINKS {
+        WB_Magazines[] = {
+            "rhsgref_50Rnd_792x57_SmE_drum",
+            "rhsgref_50Rnd_792x57_SmE_notracers_drum",
+            "rhsgref_50Rnd_792x57_SmK_drum",
+            "rhsgref_50Rnd_792x57_SmK_alltracers_drum",
+            "rhsgref_296Rnd_792x57_SmE_belt",
+            "rhsgref_296Rnd_792x57_SmE_notracers_belt",
+            "rhsgref_296Rnd_792x57_SmK_belt",
+            "rhsgref_296Rnd_792x57_SmK_alltracers_belt"
+        };
+    };
+
+    class CBA_8x22_Type14 {
+        WB_Magazines[] = {
+            "rhs_mag_6x8mm_mhp"
+        };
+    };
+
+    class M240_762x51 {
+        WB_Magazines[] = {
+            "rhsusf_50Rnd_762x51_m993",
+            "rhsusf_100Rnd_762x51_m993"
+        };
+    };
+
+    class M249_556x45 {
+        WB_Magazines[] = {
+            "rhs_200rnd_556x45_T_SAW"
+        };
+    };
+
+    class PK_762x54R {
+        WB_Magazines[] = {
+            "rhssaf_250Rnd_762x54R"
+        };
+    };
+
+    class RPG7 {
+        WB_Magazines[] = {
+            "rhs_rpg7_PG7VM_mag"
+        };
+    };
+};
